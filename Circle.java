2f0c0ea37085065848c862f827437fc4d63a1b8f
satyabrata28 <119559350+satyabrata28@users.noreply.github.com>
2022-12-01 18:37:38 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle
+{
+public static void main(String args[])
+{
+int radius = 30;
+double pie = 4.22;
+double area = radius*pie;
+System.out.println("Area of the Circle is"+area);
+}
+}
